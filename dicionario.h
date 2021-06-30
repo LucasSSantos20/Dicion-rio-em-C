9544a80bea8bed62880fba5b771fb2a98d9f8e65
LucasSSantos20 <86748532+LucasSSantos20@users.noreply.github.com>
2021-06-30 19:08:44 -0300

    Trabalho Final
    Arquivo Final, Para usuário Windows, baixar gcc para compilar código fonte da maneira correta
    Código fonte NÂO DISPONIBILIZADO!
    SOMENTE PARA LEITURA E EXECUÇÃO DO MESMO!

---
+++ dicionario.h
@@ -0,0 +1,18 @@
+#ifndef DICIONARIO
+#define DICIONARIO
+
+
+
+void inicializa();
+void carregar();
+void vizualizar();
+void escreve();
+void imprime();
+void atualiza();
+void buscar();
+void apagar();
+void corrigir();
+void ordenar();
+void estudar();
+
+#endif
